d00776f481a206466cec5df4cd35b756519f60ba
AndreySyp <sammyva199@gmail.com>
2023-12-20 17:01:25 +0300

    Добавьте файлы проекта.

---
+++ BackofficeClient/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using BackofficeClient.Views.MainWindowPages;
+using GalaSoft.MvvmLight.Command;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace BackofficeClient.ViewModels;
+
+public class MainWindowViewModel : ViewModel
+{
+    public Page _CurrentPage;
+    public Page CurrentPage
+    {
+        get => _CurrentPage;
+        set { _CurrentPage = value; OnPropertyChanged(); }
+    }
+
+    private readonly Page p1 = new Requests();
+    private readonly Page p2 = new Positions();
+
+    public ICommand p1_c => new RelayCommand(() => CurrentPage = p1);
+    public ICommand p2_c => new RelayCommand(() => CurrentPage = p2);
+}
