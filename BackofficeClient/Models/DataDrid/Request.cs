e66fd13a1ec9c1995d716b7e25e3644cf6179c34
AndreySyp <sammyva199@gmail.com>
2024-01-18 14:15:39 +0300

    123

---
+++ BackofficeClient/Models/DataDrid/Request.cs
@@ -0,0 +1,38 @@
+﻿namespace BackofficeClient.Models.DataDrid;
+
+public class Request
+{
+    public Request(long? requestId, DateOnly? requestDate, string? requestName, string? customer, string? nameShort, string? groupMtr, string? supState, long? np, long? nl, string? requestComment, string? priority, decimal? sumIncPrice, string? personManager, string tradeSign)
+    {
+        RequestId = requestId;
+        RequestDate = requestDate;
+        RequestName = requestName;
+        Customer = customer;
+        NameShort = nameShort;
+        GroupMtr = groupMtr;
+        SupState = supState;
+        Np = np;
+        Nl = nl;
+        RequestComment = requestComment;
+        Priority = priority;
+        SumIncPrice = sumIncPrice;
+        PersonManager = personManager;
+        TradeSign = tradeSign;
+    }
+
+    public long? RequestId { get; internal set; }
+    public string? RequestNum { get; internal set; }
+    public DateOnly? RequestDate { get; internal set; }
+    public string? RequestName { get; internal set; }
+    public string? Customer { get; internal set; }
+    public string? NameShort { get; internal set; }
+    public string? GroupMtr { get; internal set; }
+    public string? SupState { get; internal set; }
+    public long? Np { get; internal set; }
+    public long? Nl { get; internal set; }
+    public string? RequestComment { get; internal set; }
+    public string? Priority { get; internal set; }
+    public decimal? SumIncPrice { get; internal set; }
+    public string? PersonManager { get; internal set; }
+    public string? TradeSign { get; internal set; }
+}
--- BackofficeClient/ViewModels/RequestsViewModel.cs
+++ BackofficeClient/ViewModels/RequestsViewModel.cs
@@ -1,17 +1,79 @@
-﻿namespace BackofficeClient.ViewModels;
+﻿using CommunityToolkit.Mvvm.Input;
+using BackofficeClient.Models.DataDrid;
 
-public class RequestsViewModel
+namespace BackofficeClient.ViewModels;
+
+public class RequestsViewModel : ViewModelBase
 {
-    public IEnumerable<object> Items { get; set; }
+    public IEnumerable<Request> AllItems { get; set; } = new List<Request>();
 
-    public RequestsViewModel()
+    public IEnumerable<Request> FilteredItems { get; set; } = new List<Request>();
+
+    public string RequestNumberFilter { get; set; }
+
+    public string RequestCustomerFilter { get; set; }
+
+    public string RequestNameFilter { get; set; }
+
+    public string RequestDirectioFilter { get; set; }
+
+    public string RequestStatusFilter { get; set; }
+
+
+    public RelayCommand DataLoadingCommand => new(async () =>
     {
         using DatabaseContext db = new();
 
-        Items = db.VRequestForms.Join(db.Requests,
-            t1 => t1.RequestId,
-            t2 => t2.RequestId,
-            (t1, t2) => new { t1.RequestId, t1.RequestNum, t1.RequestDate, t1.RequestName, t1.Customer, t1.NameShort, t1.GroupMtr, t1.SupState, t1.Np, t1.Nl, t1.RequestComment, t1.Priority, t1.SumIncPrice, t1.PersonManager, t2.TradeSign })
-            .ToList();
+        await Task.Run(() =>
+            AllItems = db.VRequestForms.Join(db.Requests,
+                t1 => t1.RequestId,
+                t2 => t2.RequestId,
+                (t1, t2) => new Request(t1.RequestId, t1.RequestDate, t1.RequestName, t1.Customer, t1.NameShort, t1.GroupMtr, t1.SupState, t1.Np, t1.Nl, t1.RequestComment, t1.Priority, t1.SumIncPrice, t1.PersonManager, t2.TradeSign))
+                .ToList()
+            );
+
+        FilteredItems = AllItems;
+        OnPropertyChanged("FilteredItems");
+    });
+    public RelayCommand DataFilteredCommand => new(async () =>
+    {
+        if (AllItems == null || FilteredItems == null)
+        {
+            return;
+        }
+
+        await Task.Run(() =>
+        {
+            FilteredItems = AllItems;
+
+            if (!string.IsNullOrWhiteSpace(RequestNumberFilter))
+            {
+                FilteredItems = FilteredItems.Where(x => x.RequestNum == RequestNumberFilter);
+            }
+            if (!string.IsNullOrWhiteSpace(RequestCustomerFilter))
+            {
+                FilteredItems = FilteredItems.Where(x => x.Customer == RequestCustomerFilter);
+            }
+            if (!string.IsNullOrWhiteSpace(RequestNameFilter))
+            {
+                FilteredItems = FilteredItems.Where(x => x.RequestName == RequestNameFilter);
+            }
+            if (!string.IsNullOrWhiteSpace(RequestDirectioFilter))
+            {
+                FilteredItems = FilteredItems.Where(x => x.PersonManager == RequestDirectioFilter);
+            }
+            if (!string.IsNullOrWhiteSpace(RequestStatusFilter))
+            {
+                //var t = FilteredItems.First().TradeSign;
+                //var c = RequestStatusFilter;
+                FilteredItems = FilteredItems.Where(x => x.TradeSign == RequestStatusFilter);
+            }
+        });
+
+        OnPropertyChanged("FilteredItems");
+    });
+    public RequestsViewModel()
+    {
+
     }
 }
