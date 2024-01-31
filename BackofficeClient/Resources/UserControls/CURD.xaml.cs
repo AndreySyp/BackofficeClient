290833cec68c7903812217d43fe4bdd9481eeb3c
AndreySyp <sammyva199@gmail.com>
2024-01-31 15:19:47 +0300

    asd

---
+++ BackofficeClient/Resources/UserControls/CURD.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BackofficeClient.Resources.UserControls
+{
+    /// <summary>
+    /// Логика взаимодействия для CURD.xaml
+    /// </summary>
+    public partial class CURD : UserControl
+    {
+        public Binding ASD = new("Value") { ElementName = "dataGrid" };
+        public CURD()
+        {
+            InitializeComponent();
+        }
+    }
+}
